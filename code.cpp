1d736998874172d236ddc192ffce076c32d73137
Rasel477867 <113754632+Rasel477867@users.noreply.github.com>
2023-01-17 21:46:52 +0600

    NOTATRI - Not a Triangle-Solution
    https://www.spoj.com/problems/NOTATRI/

--- code.cpp
+++ code.cpp
@@ -1,56 +1,51 @@
-
-
-
-
 #include<bits/stdc++.h>
 using namespace std;
 #define endl '\n'
 #define ll long long int
 #define pb push_back
-ll n,m;
+#define ps push
+#define pp pop
+map<ll,vector<ll> >m1;
+map<ll,ll>m;
+
+
+
 vector<ll>v;
-bool cheak(ll num)
-{
-    ll sum=0,i;
-    for(i=0; i<n; i++)
-    {
-        if(v[i]>num)
-            sum+=v[i]-num;
-    }
-    if(sum>=m)
-       return true;
-    else
-        return false;
-}
+
 int main()
 {
     ios::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
-    ll i,ans,mid,b,a,r,l;
-    cin>>n>>m;
-    b=0;
-    for(i=0; i<n; i++)
-    {
-        cin>>a;
-        v.pb(a);
-        if(a>b)
-            b=a;
-    }
-    l=0;
-    r=b;
-    while(r>=l)
-    {
-        mid=(l+r)/2;
-        if(cheak(mid))
-        {
-            ans=mid;
-            l=mid+1;
-        }
-        else
-            r=mid-1;
-    }
-    cout<<ans;
+    ll t,i,n,ans,j,a,b,c,p=1,sum;
 
-    return 0;
+    while(1)
+    {
+        cin>>n;
+        if(n==0)
+            break;
+        v.clear();
+        ans=0;
+        for(i=0; i<n; i++)
+        {
+            cin>>a;
+            v.pb(a);
+        }
+        sort(v.begin(),v.end());
+        for(i=0; i<n-2; i++)
+        {
+            for(j=i+1; j<n-1; j++)
+            {
+                a=v[i];
+                b=v[j];
+                sum=a+b;
+                c=upper_bound(v.begin()+j+1,v.end(),sum)-v.begin();
+                ans+=n-c;
+            }
+        }
+       cout<<ans<<endl;
+
+
+    }
+return 0;
 }
