a64cf42d50b6f044fe7e14c62caffb66b6245fd3
Rasel477867 <113754632+Rasel477867@users.noreply.github.com>
2023-01-17 07:34:16 +0600

    MINSTOCK - Minimum Stocks/solution
    https://www.spoj.com/problems/MINSTOCK/en/

--- cp.cpp
+++ cp.cpp
@@ -5,36 +5,64 @@ using namespace std;
 #define pb push_back
 #define ps push
 #define pp pop
-map<string,ll >m;
-map<string,ll >:: iterator it;
-vector<ll>v;
+#include <ext/pb_ds/assoc_container.hpp> // Common file
+#include <ext/pb_ds/tree_policy.hpp>
+#include <functional> // for less
+#include <iostream>
+using namespace __gnu_pbds;
+using namespace std;
+
+typedef tree<long long int, null_type, less_equal<long long int>, rb_tree_tag,
+             tree_order_statistics_node_update>
+    ordered_multiset;
+
+map<ll,string>m;
+map<ll,string>:: iterator it;
+map<ll,string>:: iterator it1;
+map<string,ll>m1;
+map<string,ll>:: iterator it2;
 int main()
 {
     ios::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
-    ll n,i,k,a,key,sum;
+    ll i,t,n,a,b;
     string s;
-    cin>>n>>k;
-    for(i=0; i<n; i++)
+    cin>>t;
+    i=0;
+    while(t--)
     {
-        cin>>s>>key;
-        m[s]+=key;
+        i++;
+        cin>>n>>s;
+        if(s!="BUY")
+        {
+            cin>>b;
+            if(n==1)
+            {
+                m[b]=s;
+                m1[s]=b;
+            }
+            else{
+                a=m1[s];
+
+                m.erase(a);
+                m1[s]=b;
+                m[b]=s;
+
+
+            }
+        }
+        else{
+            it=m.begin();
+            cout<<it->second<<" "<<i<<endl;
+            m1.erase(it->second);
+            m.erase(it);
+
+        }
+
+
     }
-    for(it=m.begin(); it!=m.end(); it++)
-        v.pb(it->second);
-    sort(v.begin(),v.end());
-    reverse(v.begin(),v.end());
-    sum=0;
-    for(i=0; i<v.size(); i++)
-    {
-        if(i==k)
-            break;
-        sum+=v[i];
-    }
-cout<<sum;
 
     return 0;
+
 }
-
-
