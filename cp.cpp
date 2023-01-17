ce0254db7ecab6921bcfae1b04154853e16a3f29
Rasel477867 <113754632+Rasel477867@users.noreply.github.com>
2023-01-17 08:23:26 +0600

    Hacking the random number generator-solution
    https://www.spoj.com/problems/HACKRNDM/en/

--- cp.cpp
+++ cp.cpp
@@ -5,64 +5,37 @@ using namespace std;
 #define pb push_back
 #define ps push
 #define pp pop
-#include <ext/pb_ds/assoc_container.hpp> // Common file
-#include <ext/pb_ds/tree_policy.hpp>
-#include <functional> // for less
-#include <iostream>
-using namespace __gnu_pbds;
-using namespace std;
+vector<ll>v;
 
-typedef tree<long long int, null_type, less_equal<long long int>, rb_tree_tag,
-             tree_order_statistics_node_update>
-    ordered_multiset;
+map<ll,ll>m;
 
-map<ll,string>m;
-map<ll,string>:: iterator it;
-map<ll,string>:: iterator it1;
-map<string,ll>m1;
-map<string,ll>:: iterator it2;
+
 int main()
 {
     ios::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
-    ll i,t,n,a,b;
-    string s;
-    cin>>t;
-    i=0;
-    while(t--)
+    ll n,i,a,k,ans=0;
+    cin>>n>>k;
+    for(i=0; i<n; i++)
     {
-        i++;
-        cin>>n>>s;
-        if(s!="BUY")
+        cin>>a;
+        m[a]+=1;
+        v.pb(a);
+    }
+    for(i=0; i<v.size(); i++)
+    {
+        a=v[i]-k;
+        if(m[a]==1)
         {
-            cin>>b;
-            if(n==1)
-            {
-                m[b]=s;
-                m1[s]=b;
-            }
-            else{
-                a=m1[s];
-
-                m.erase(a);
-                m1[s]=b;
-                m[b]=s;
-
-
-            }
-        }
-        else{
-            it=m.begin();
-            cout<<it->second<<" "<<i<<endl;
-            m1.erase(it->second);
-            m.erase(it);
+            ans++;
 
         }
-
 
     }
+    cout<<ans;
+
+
 
     return 0;
-
 }
