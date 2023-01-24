2ded3981c5c24878cc95aa7baf32b80c975fdced
Rasel477867 <113754632+Rasel477867@users.noreply.github.com>
2023-01-24 12:18:13 +0600

    ARRAYSUB - subarrays- soluiton
    https://www.spoj.com/problems/ARRAYSUB/

--- code.cpp
+++ code.cpp
@@ -1,51 +1,58 @@
+#include <ext/pb_ds/assoc_container.hpp> // Common file
+#include <ext/pb_ds/tree_policy.hpp>
+#include <functional> // for less
+#include <iostream>
 #include<bits/stdc++.h>
-using namespace std;
 #define endl '\n'
 #define ll long long int
 #define pb push_back
-#define ps push
-#define pp pop
-map<ll,vector<ll> >m1;
+using namespace __gnu_pbds;
+using namespace std;
+
+typedef tree<long long int, null_type, less_equal<long long int>, rb_tree_tag,
+        tree_order_statistics_node_update>
+        ordered_multiset;
+ordered_multiset s;
+//ordered_multiset :: iterator it;
 map<ll,ll>m;
-
-
-
+map<ll,ll>:: iterator it;
 vector<ll>v;
-
 int main()
 {
     ios::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
-    ll t,i,n,ans,j,a,b,c,p=1,sum;
-
-    while(1)
+    ll n,k,a,b,i;
+    cin>>n;
+    for(i=0; i<n; i++)
     {
-        cin>>n;
-        if(n==0)
-            break;
-        v.clear();
-        ans=0;
-        for(i=0; i<n; i++)
-        {
-            cin>>a;
-            v.pb(a);
-        }
-        sort(v.begin(),v.end());
-        for(i=0; i<n-2; i++)
-        {
-            for(j=i+1; j<n-1; j++)
-            {
-                a=v[i];
-                b=v[j];
-                sum=a+b;
-                c=upper_bound(v.begin()+j+1,v.end(),sum)-v.begin();
-                ans+=n-c;
-            }
-        }
-       cout<<ans<<endl;
-
-
+        cin>>a;
+        v.pb(a);
     }
-return 0;
+    cin>>k;
+    ll r=0,l=0;
+    for(i=0; i<n; i++)
+    {
+        a=v[i];
+        r++;
+        m[a]+=1;
+        if(r-l==k)
+        {
+            it=m.end();
+            it--;
+            cout<<it->first<<" ";
+            a=v[l];
+            m[a]-=1;
+            if(m[a]==0)
+            {
+                m.erase(a);
+            }
+            l++;
+        }
+    }
+
+
+    return 0;
+
 }
+
