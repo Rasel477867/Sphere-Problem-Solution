2a110626f26622d29d8271ff087dbb5093a3c312
Rasel477867 <113754632+Rasel477867@users.noreply.github.com>
2023-02-07 09:34:07 +0600

    TIPTOP - Tip Top Game
    https://www.spoj.com/problems/TIPTOP/

--- code.cpp
+++ code.cpp
@@ -1,10 +1,11 @@
+
 #include <ext/pb_ds/assoc_container.hpp> // Common file
 #include <ext/pb_ds/tree_policy.hpp>
 #include <functional> // for less
 #include <iostream>
 #include<bits/stdc++.h>
 #define endl '\n'
-#define ll  long long int
+#define ll long long int
 #define pb push_back
 
 
@@ -14,12 +15,28 @@ using namespace std;
 typedef tree<long long int, null_type, less_equal<long long int>, rb_tree_tag,
         tree_order_statistics_node_update>
         ordered_multiset;
-ordered_multiset s;
+ordered_multiset s1;
 //ordered_multiset :: iterator it;
-
-const ll N=1e6;
+const ll mod=1e9+7;
+const ll z=1e6+5;
+ll fact[z];
+const ll N=1e1;
  bool sive[N];
  vector<ll>prime;
+ vector<ll>v;
+ set<ll>s;
+ set<ll>:: iterator its;
+ map<ll,ll >m;
+ map<ll,ll >:: iterator it;
+ ll ex(ll a,ll b,ll mod)
+{
+    if(b==0)
+        return 1;
+    else if(b%2==0)
+        return (ex((a*a)%mod,b/2,mod));
+    else
+        return (a*ex((a*a)%mod,(b-1)/2,mod))%mod;
+}
  void segsive(ll l,ll r)
  {    ll base,i,j;
      bool sprime[r-l+1];
@@ -46,6 +63,37 @@ const ll N=1e6;
             }
        }
        cout<<endl;
+ }
+  ll euler_pi_function(ll n)
+ { ll num,num1,num2,i;
+      num=n;
+     num1=n;
+    for(i=0; prime[i]*prime[i]<=num ; i++)
+    {
+        while(1)
+        {
+            if(n%prime[i]!=0)
+                break;
+            else{
+                n/=prime[i];
+                m[prime[i]]++;
+            }
+        }
+    }
+    if(n>1)
+        m[n]++;
+
+
+       num=1;
+      num2=1;
+     for(it=m.begin(); it!=m.end(); it++)
+     {
+         num=num*(it->first);
+         num2=num2*((it->first)-1);
+     }
+     num=num1/num;
+     num=num*num2;
+   return num;
  }
 
 int main()
@@ -75,13 +123,26 @@ int main()
        if(sive[i]==true)
            prime.pb(i);
     }
-    ll l,r,n,t;
-    cin>>t;
-    while(t--)
-    {
-          cin>>l>>r;
-          segsive(l,r);
-    }
+   fact[0]=1;
 
+   for(i=1; i<z; i++)
+   {
+       fact[i]=(fact[i-1]*i)%mod;
+
+   }
+   ll n,ans,t;
+   cin>>t;
+   ll k=1;
+   while(t--)
+   {
+       cin>>n;
+       ans=sqrt(n);
+       if(ans*ans==n)
+           cout<<"Case "<<k<<": "<<"Yes"<<endl;
+       else{
+        cout<<"Case "<<k<<": "<<"No"<<endl;
+       }
+       k++;
+   }
    return 0;
 }
